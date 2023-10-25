8808421388ad2c1e72ac19583e5b19a5abd0c749
Bedasa <48552699+bedasawayessa@users.noreply.github.com>
2023-10-25 09:57:45 +0300

    here

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-// reads and prints the content of a file.
 
-const file = process.argv[2];
-const fileStream = require('fs');
-fileStream.readFile(file, 'utf-8', (error, data) => {
-  if (error) {
-    console.log(error);
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
   } else {
-    console.log(data);
+    process.stdout.write(data);
   }
 });
