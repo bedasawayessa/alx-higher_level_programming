376c0dcefe84b23defadf14dbea2f70f2b528252
Bedasa <48552699+bedasawayessa@users.noreply.github.com>
2024-03-27 10:31:16 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
 
-const request = require('request');
-const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+const req = require('request');
+const fileStream = require('fs');
 
-request(process.argv[2], function (_err, _res, body) {
-  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+req(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fileStream.writeFile(file, body, 'utf-8', (error) => {
+      if (error) console.log(error);
+    });
+  }
 });
